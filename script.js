bc82a314cf9d06781b50990b24ad8d40933f3773
Elyse Wells <cewells89@gmail.com>
2020-10-10 02:05:27 -0400

    began scaffolding out html page; starter pseudocode for JS logic

---
+++ script.js
@@ -0,0 +1,5 @@
+// display user's local weather when first loading the page
+//      to be retrieved from the browser information
+// cities searched by the user should remain in a list on the UI
+//      user should be able to click items in the list to retrieve weather information for that city
+// save cities to local storage
